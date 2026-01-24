e27d87f29f9383f5ce66301de358c8ad2789af01
Valeria00 <astrakhantseva.00@mail.ru>
2026-01-24 22:51:19 +0300

    Структуризация кода

--- sandbox/src/main/java/ru/srqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/srqa/geometry/figures/Rectangle.java
@@ -2,7 +2,8 @@ package ru.srqa.geometry.figures;
 
 public class Rectangle {
     public static void printRectangleArea(double side1, double side2) {
-        System.out.println("Площадь прямоугольника со сторонами " + side1 + " и " + side2 + " = " + RectangleArea(side1, side2));
+        var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", side1, side2, RectangleArea(side1, side2));
+        System.out.println(text);
     }
 
     private static double RectangleArea(double side1, double side2) {
--- sandbox/src/main/java/ru/srqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/srqa/geometry/figures/Square.java
@@ -6,6 +6,7 @@ public class Square {
     }
 
     public static void printSquareArea(double side) {
-        System.out.println("Площадь квадрата со стороной " + side + " = " + squareArea(side));
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, squareArea(side));
+        System.out.println(text);
     }
 }
