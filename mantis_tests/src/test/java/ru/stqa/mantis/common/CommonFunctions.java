59ad6a7c173bf2518738d5b9aac5541d0c3769ea
Valeria00 <astrakhantseva.00@mail.ru>
2026-03-17 00:51:46 +0300

    Почтовый сервер James и управление через интерфейс командной строки

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/common/CommonFunctions.java
@@ -0,0 +1,28 @@
+package ru.stqa.mantis.common;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CommonFunctions {
+    public static String randomString(int n) {
+        var rnd = new Random(); //ф-ция рандомайзер
+        Supplier<Integer> randomNumbers = () -> rnd.nextInt(26);
+        var result = Stream.generate(randomNumbers)
+                .limit(n)
+                .map(i -> 'a' + i)
+                .map(i -> Character.toString(i))
+                .collect(Collectors.joining());
+        return result;
+    }
+
+    public static String randomFile(String dir) {
+        var fileNames = new File(dir).list();
+        var rnd = new Random();
+        var index = rnd.nextInt(fileNames.length);
+        return Paths.get(dir, fileNames[index]).toString();
+    }
+}
